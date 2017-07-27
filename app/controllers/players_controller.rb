b41c1d9b9c8dbc0d330532631a135d2860b834e1
Jess Bakkers <jessica.bakkers@gmail.com>
2017-07-27 15:43:50 -0700

    submit new player redirects to players show page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -15,7 +15,7 @@ class PlayersController < ApplicationController
     def create
         @player = Player.new(player_params)
         if @player.save
-            redirect_to players_path
+            redirect_to player_path(@player)
         else
             render :new
         end
